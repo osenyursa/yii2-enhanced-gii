5556c98305e7f0d38816b0ee6d76fb3404bd0b57
张景贤 <osenyursa@gmail.com>
2017-01-18 17:59:23 +0800

    Update model.php

--- model/default/model.php
+++ model/default/model.php
@@ -69,7 +69,7 @@ class <?= $className ?> extends <?= ($isTree) ? '\kartik\tree\models\Tree' . "\n
      */
     public static function tableLabel()
     {
-        return '<?= $tableLabel ?>';
+        return '<?= $generator->getTableLabel() ?>';
     }
 <?php if ($generator->db !== 'db'): ?>
 
